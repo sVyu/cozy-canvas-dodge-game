171f76cda3dfb2bfb422ae5cb0507f77e6e4747b
vyu <clearvyu@naver.com>
2025-05-05 21:58:52 +0900

    feat: stop and enter to continue

--- scripts/game_manager.js
+++ scripts/game_manager.js
@@ -2,7 +2,19 @@ import { mainScript } from './main_script.js';
 
 const GameManager = async () => {
   for (let i = 0; i < 3; ++i) {
+    // mainScript for 1 round
     const passedTime = await mainScript();
+
+    // EnterToContinue
+    await new Promise((resolve) => {
+      const EnterkeyCheck = (e) => {
+        if (e.keyCode == 13) {
+          window.removeEventListener('keydown', EnterkeyCheck, false);
+          resolve();
+        }
+      };
+      window.addEventListener('keydown', EnterkeyCheck, false);
+    });
   }
 };
 
