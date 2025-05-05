bcda8d2bf66187099541cdbd09ea397dc42909c9
vyu <clearvyu@naver.com>
2025-05-05 22:55:00 +0900

    feat: toggle pressEnterContinueBox with transition animation

--- scripts/game_manager.js
+++ scripts/game_manager.js
@@ -1,15 +1,18 @@
 import { mainScript } from './main_script.js';
 
 const GameManager = async () => {
-  for (let i = 0; i < 3; ++i) {
+  const $PressEnterToContinue = document.querySelector('#pressEnterToContinue');
+
+  for (let i = 0; i < 100; ++i) {
     // mainScript for 1 round
     const passedTime = await mainScript();
-
     // EnterToContinue
     await new Promise((resolve) => {
+      $PressEnterToContinue.classList.toggle('show');
       const EnterkeyCheck = (e) => {
         if (e.keyCode == 13) {
           window.removeEventListener('keydown', EnterkeyCheck, false);
+          $PressEnterToContinue.classList.toggle('show');
           resolve();
         }
       };
